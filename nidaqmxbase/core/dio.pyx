bd0794b244ebfa0f47894237e342d26cc33aefea
Sean Vig <sean.v.775@gmail.com>
2013-04-03 09:28:10 -0500

    x

--- nidaqmxbase/core/dio.pyx
+++ nidaqmxbase/core/dio.pyx
@@ -25,14 +25,40 @@ cdef class DITask(Task):
         wrap_error(DAQmxBaseCreateDIChan(self.handle, lines, "", DAQmx_Val_ChanForAllLines))
         nchans += 1
 
-    cdef uInt32[] read(Task self, int32 numSamples, float64 timeout=10., bool32 fillMode=DAQmx_Val_GroupByChannel):
+    cdef uInt32[] read(Task self, int32 numSamples, float64 timeout=10.,
+            bool32 fillMode=DAQmx_Val_GroupByChannel):
+        """
+        Read in a single reading from added channels
+
+        Parameters
+        ----------
+
+        numSamples : int32
+            The number of samples, per channel, to read. The default value of
+            -1 (DAQmx_Val_Auto) reads all available data. If readArray does not
+            contain enough space, this function returns as much data as fits in
+            readArray.
+
+        timeout : float64
+            The amount of time, in seconds, to wait for the function to read
+            the sample(s). This function returns an error if the timeout
+            elapses. Default: 10.
+
+        fillMode : bool32
+            Specifies whether or not the samples are interleaved.
+            DAQmx_Val_GroupByChannel: Group by channel (non-interleaved).
+            DAQmx_Val_GroupByScanNumber: Group by scan number (interleaved).
+            Default: DAQmx_Val_GroupByChannel)
+        """
+
         cdef uInt32 readArray[nchans]
         cdef int32 sampsPerChanRead
 
         if not self._started:
             # TODO: raise error
             pass
-        wrap_error(DAQmxBaseReadDigitalU32(self.handle, self._nchans, timeout, fillMode, &sampsPerChanRead, NULL))
+        wrap_error(DAQmxBaseReadDigitalU32(self.handle, self._nchans, timeout,
+            fillMode, readArray, 1, &sampsPerChanRead, NULL))
 
         return readArray
 
