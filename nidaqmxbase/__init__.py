720bf6c6c9a13aa6707cfa224863829db9d64fd8
Sean Vig <sean.v.775@gmail.com>
2014-01-09 14:50:09 -0600

    Rearrange author/license information

--- nidaqmxbase/__init__.py
+++ nidaqmxbase/__init__.py
@@ -5,7 +5,9 @@ import ctypes
 # This is necessary because of NI library annoyances
 ctypes.CDLL("/usr/local/lib/liblvrtdark.so.12.0", mode=ctypes.RTLD_GLOBAL)
 
-__version__ = "0.0.2-git"
+__author__ = 'Sean Vig <seanvig2@illinois.edu>'
+__license__ = 'BSD'
+__version__ = '0.0.2-git'
 
 # Top-level imports
 from nidaqmxbase import core
